1c99f7c4e3e2e93dab8108756b7d9ee4d02acffb
cshao <33412583+shaochennjtu@users.noreply.github.com>
2022-09-19 11:16:25 +0800

    Update migrate_vm.py

--- python/migrate_vm.py
+++ python/migrate_vm.py
@@ -6,15 +6,11 @@ Make sure Migrate VMs are on Powerdown or Pause status.
 ”“”
 
 import os
+import time
+import logging
 from ..helpers import rhvm_api
 
-log = logging.getLogger('bender')
-
-
-api_url = "https://{rhevm_fqdn}/ovirt-engine/api/{item}"
-user = ''
-password = ''
-domain=''
+log = logging.getLogger('migrate')
 
 old_storage_id = ''
 new_storage_id = ''
@@ -24,21 +20,6 @@ search_query = ''
 
 class Migration(object):
     """"""
-
-    # connect to rhvm api
-    def connect(api_url, user, password):
-        VERSION = params.Version(major='4', minor='5')
-        rhvm_api = API(url=api_url, user=user, password=password, doamin=internal)
-        return rhvm_api
-
-
-    # append vm to migration vm list
-    def get_vms_to_migrate(rhvm_api, search_query):
-        vms_migrate_list = []
-        for vm in rhvm_api.vms.list(query=search_query):
-            print("'{}' is set to be migrated.".format(vm.name))
-            vms_migrate_list.append(vm)
-        return vms_migrate_list
 
     # start deactivate vm disk
     def deactivate_disk(vm, disk):
@@ -51,7 +32,7 @@ class Migration(object):
 
     # create new image for VM
     def create_nfs_disk(rhvm_api, new_storage_id, disk, vm):
-        print("[{}] Creating an NFS image for '{}'...".format(vm.name, disk.name))
+        print("[{}] Creating an NFS disk for '{}'...".format(vm.name, disk.name))
         new_storage_domain = rhvm_api.storagedomains.get(id=new_storage_id)
         disk_params = params.Disk()
         disk_params.set_alias(disk.name)
@@ -96,39 +77,30 @@ class Migration(object):
 
 
     # start to migrate vm disks
-    def migrate_disks(rhvm_api, vms_to_migrate, old_storage_id, new_storage_id, nfs_mount_dir):
+    def migrate_disks(rhvm_api, old_storage_id, new_storage_id, nfs_mount_dir):
         completed_vms = []
         failed_vms = []
-        for vm in vms_to_migrate:
-            print("Starting migration for '{}'.".format(vm.name))
-            disks = vm.disks.list()
-            for disk in disks:
-                for storage_domain in disk.storage_domains.storage_domain:
-                    if storage_domain.id == old_storage_id:
-                        print("[{}] '{}' needs to be migrated...".format(vm.name, disk.name))
-                        try:
-                            deactivate_disk(vm, disk)
-                            print("[{}] Attempting to migrate '{}' to NFS...".format(vm.name, disk.name))
-                            new_disk = create_nfs_disk(rhvm_api, new_storage_id, disk, vm)
-                            print("[{}] Converting '{}' from NFS to NFS...".format(vm.name, disk.name))
-                            image_path = find_image(new_storage_id, new_disk, nfs_mount_dir)
-                            if image_path:
-                                if os.system("qemu-img convert -f qcow -O raw nfs:{}/volume-{}:id={}:conf={} {}".format(
-                                    image_path)) == 0:
-                                    attach_detach_disk(vm, disk, new_disk)
-                                    attach_detach_disk(vm, disk, new_disk)
-                                    print("[{}] Sucessfully migrated '{}'!".format(vm.name, disk.name))
-                                else:
-                                    print("[{}] Failed to convert '{}' from NFS to NFS.".format(vm.name, disk.name))
-                                    print("[{}] Failed to convert '{}' from NFS to NFS.".format(vm.name, disk.name))
-                                    error_message(vm, disk, failed_vms)
+        if storage_domain.id == old_storage_id:     #Judge whether the new storage are same with old storage, will not move if same.
+            print("[{}] '{}' needs to be migrated...".format(vm.name, disk.name))
+            try:
+                deactivate_disk(vm, disk)
+                print("[{}] Deactivating disk '{}' for migration...".format(vm.name, disk.name))
+                new_disk = create_nfs_disk(rhvm_api, new_storage_id, disk, vm)
+                print("[{}] Creating an NFS disk for '{}'...".format(vm.name, disk.name))
+                image_path = find_image(new_storage_id, new_disk, nfs_mount_dir)
+                if image_path:
+                    if os.system("qemu-img convert -f qcow -O raw nfs:{}/volume-{}:id={}:conf={} {}".format(
+                        image_path)) == 0:
+                        attach_detach_disk(vm, disk, new_disk)
+                        print("[{}] Sucessfully migrated '{}'!".format(vm.name, disk.name))
+                    else:
+                        error_message(vm, disk, failed_vms)
+                else:
+                    error_message(vm, disk, failed_vms)
+            except:
+                error_message(vm, disk, failed_vms)
+        return completed_vms, failed_vms
 
-                            else:
-                                print("[{}] Could not find the correct image file to convert.".format(vm.name))
-                                error_message(vm, disk, failed_vms)
-                        except:
-                            error_message(vm, disk, failed_vms)
-                 return completed_vms, failed_vms
 
     print("No more VMs to migrate.")
     os.remove('.rhv_migration_lock')
